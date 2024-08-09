64c3dcc04d9de9c0a52840bfebf99860435b017a
Lovlin Thakkar <lovlinthakkar99@gmail.com>
2024-08-09 13:19:33 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/xkcd-info', async (req, res) => {
+    try {
+        const response = await fetch('https://xkcd.com/info.0.json');
+        const latestComic = await response.json();
+        res.json(latestComic);
+    } catch (error) {
+        res.status(500).send('Error fetching XKCD comic');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('CORS proxy running on port 3000');
+});
